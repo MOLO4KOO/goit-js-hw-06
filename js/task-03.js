318ae87def1b0771c4c0e1d003a7ec1c2e43cbe3
MOLO4KOO <94447118+MOLO4KOO@users.noreply.github.com>
2022-07-12 23:41:57 +0300

    goit-js-hw-06

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,8 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const galleryEl = document.querySelector('.gallery')
+const liEl = images.map( image => `<li class="gallery-item"><img src="${image.url}" 
+alt="${image.alt}" width="400"</li>`)
+  .join("");
+  galleryEl.insertAdjacentHTML("beforeend", liEl);
